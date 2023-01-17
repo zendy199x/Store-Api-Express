57dcc1049d1a9d65bd3116173c470818eb19c9f9
Zendy <nguyenduc.051194@gmail.com>
2023-01-18 00:26:30 +0700

    Define routes

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Store API");
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+// async errors
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use("/api/v1/products", productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+mongoose.set("strictQuery", false);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening port ${port}...`));
+  } catch (error) {}
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-}
+  return mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to DB!"))
+    .catch((error) => console.log(error));
+};
 
-module.exports = connectDB
+module.exports = connectDB;
